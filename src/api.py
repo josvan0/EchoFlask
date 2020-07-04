5f78e83fb26945813183fa8373f868eef626107a
Josue Rodriguez <josvanrh09@gmail.com>
2020-07-04 12:35:45 -0500

    add comments, refactor Resources and paths API

--- src/api.py
+++ src/api.py
@@ -5,23 +5,29 @@ from flask import Flask
 from flask_restful import Api, Resource
 
 
+# initialize a Flask app (a server)
 app = Flask(__name__)
+# implements the Api class on the app
 api = Api(app)
 
 
-class HelloWorld(Resource):
-    def get(self):
-        return { 'message': 'Hello World' }
+# create a resource api with Resource class
+class Hello(Resource):
+    # define method GET with keyword params (optionals)
+    def get(self, some=None):
+        # return object Python as JSON
+        return {
+            'message': f'Hello {some}'
+        } if some else {
+            'message': 'Hello World'
+        }
 
 
-class HelloSome(Resource):
-    def get(self, some):
-        return { 'message': f'Hello {some}' }
-
-
-api.add_resource(HelloWorld, '/')
-api.add_resource(HelloSome, '/<string:some>')
+# add the resource to api
+# resource it's used to paths specified
+api.add_resource(Hello, '/', '/<string:some>', endpoint='hello')
 
 
 if __name__ == '__main__':
+    # start server on port selected
     app.run(port='9000')
